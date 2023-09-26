6796c50277ca0b7ea6d5faf0526ff647f5b853a1
Laryd <hillarymuboka@gmail.com>
2023-09-26 18:49:38 +0300

    swiper

--- src/components/slider/SwiperF.jsx
+++ src/components/slider/SwiperF.jsx
@@ -11,7 +11,6 @@ import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 const SwiperF = () => {
   return (
     <div className="container">
-      <h1 className="heading">Flower Gallery</h1>
       <Swiper
         effect={"coverflow"}
         grabCursor={true}
@@ -34,33 +33,36 @@ const SwiperF = () => {
         className="swiper_container"
       >
         <SwiperSlide>
-          <img src="./assets/images/img_1.jpg" alt="slide_image" />
+          <img
+            src="src\assets\images\img_1.jpg"
+            alt="slide_image"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img src='./assets/images/img_1.jpg' alt="slide_image" />
+          <img src="src/assets/images/img_2.jpg" alt="slide_image" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src='./assets/images/img_1.jpg' alt="slide_image" />
+          <img src="src/assets/images/img_3.jpg" alt="slide_image" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src='./assets/images/img_1.jpg' alt="slide_image" />
+          <img src="src/assets/images/img_4.jpg" alt="slide_image" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src='./assets/images/img_1.jpg' alt="slide_image" />
+          <img src="src/assets/images/img_5.jpg" alt="slide_image" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={slide_image_6} alt="slide_image" />
+          <img src="src/assets/images/img_6.png" alt="slide_image" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={slide_image_7} alt="slide_image" />
+          <img src="src/assets/images/img_7.jpg" alt="slide_image" />
         </SwiperSlide>
 
         <div className="slider-controler">
           <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
+            <i name="arrow-back-outline"></i>
           </div>
           <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
+            <i name="arrow-forward-outline"></i>
           </div>
           <div className="swiper-pagination"></div>
         </div>
